d5e552002549eab171ec9ccc6ee4be0a88c6af0b
Marcos Sánchez <marcosdsanchez@gmail.com>
2016-02-22 20:33:28 -0300

    Day 4, Exercise 2

---
+++ 4/4.2.cr
@@ -0,0 +1,12 @@
+require "crypto/md5"
+
+input = File.read("#{__DIR__}/input")
+i = 0
+hash = ""
+loop do
+  hash = Crypto::MD5.hex_digest("#{input.strip + i.to_s}")
+  break if hash.starts_with? "000000"
+
+  i += 1
+end
+puts "#{i}"
